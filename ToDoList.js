d24a8b3633e944ed48ce61126d560380342a9556
엄혜인 <mickey_8688@naver.com>
2024-04-12 17:45:59 +0900

    second commit

--- ToDoList.js
+++ ToDoList.js
@@ -1,3 +1,9 @@
+function ToDoList() {
+  alert("등록")
+}
+
+
+
 function AddListUp() {
   let contens = $('#contens').val();
 
@@ -6,12 +12,35 @@ function AddListUp() {
   <div class="list_group">
     <ul id="list_container" class="todo-list">
     <li class="list">
-    <div class="bx bx-leaf" id="checkbox"></div>
-    <div class="todolist_container">${contens}</div>
+    <div class="checkbox_title"><div class="checkbox"><img class="flower"></div></div>
+    <div class="todolist_container">${contens}</div>\
+    <button class="upd_btn">수정</button>
     <button class="del_btn">삭제</button>
   </li>
     </ul>
   </div>
   </div>`;
   $('#listupdate_delete').append(temp_html);
+  $('#contens').val('');
+
+}
+
+/*
+listContainer.addEventListener("click", function(e) {
+  if(e.target.tagName === "LI") {
+    e.target.classList.toggle("checked");
+  }
+  else if(e.target.tagName === "SPAN") {
+    e.target.parentElement.remove();
+  }
+}. false);
+*/
+
+function DelList() {
+  alert('삭제');
+}
+
+
+function UpdList() {
+  alert('수정');
 }
